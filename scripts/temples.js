38d5002094be20c55f5b32c807d63e33f228414f
kkevinboabilwe <anotheronekkb@gmail.com>
2026-05-23 23:42:27 +0200

    modified

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+})
